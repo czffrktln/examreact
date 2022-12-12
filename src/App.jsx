6556d4f2b06f6b217b791ca7500913e21e0f7158
Katalin Czifferi <katica@Katalins-Air.lan>
2022-12-12 10:25:42 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+    const response = await axios.get('https://reqres.in/api/users?page=1')
+    setData(response.data.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <div className="App">
- 
+      <input type="text" placeholder='input' value={input} onInput={(e) => setInput(e.target.value)}/>
     </div>
   )
 }
